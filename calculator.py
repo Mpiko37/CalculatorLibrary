12c95ac8ec84b6acaeb10ba6587153ade7331717
clone <consolationkanga@gmail.com>
2024-12-07 23:51:34 +0100

    ajout de condition if second_term ==0 à la division; autocorrection pep8 des scripts

--- calculator.py
+++ calculator.py
@@ -8,3 +8,9 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def division(first_term, second_term):
+    if second_term == 0:
+        raise ValueError("Cannot divide by zero")
+    return first_term / second_term
